a14f99b311b74f5518b3e907269dd816245842ec
Yerong <yerong.li@outlook.com>
2023-10-30 23:41:35 -0500

    Save to file

--- datasets/news/parse.py
+++ datasets/news/parse.py
@@ -1,29 +1,29 @@
-# Read embeddings from emb_news_category_w.txt and create a dictionary
-embeddings = {}
-with open('emb_news_category_w.txt', 'r') as emb_file:
-    for line in emb_file:
-        if len(line.strip()) > 0:
-            items = line.split()
-            word = items[0]
-            vector = ' '.join(items[1:])
-            embeddings[word] = vector
+# Read topic embeddings from emb_news_category_t.txt and create a dictionary
+topic_embeddings = {}
+with open('emb_news_category_t.txt', 'r') as topic_file:
+    for line in topic_file:
+        items = line.split()
+        topic = items[0]
+        vector = ' '.join(items[1:])
+        topic_embeddings[topic] = vector
+
+# Read word embeddings from emb_news_category_w.txt and create a dictionary
+word_embeddings = {}
+with open('emb_news_category_w.txt', 'r') as word_file:
+    for line in word_file:
+        items = line.split()
+        word = items[0]
+        vector = ' '.join(items[1:])
+        word_embeddings[word] = vector
 
 # Read words from res_news_category.txt and find their embeddings
 categories = {}
-with open('res_news_category.txt', 'r') as res_file:
-    lines = res_file.readlines()
-    for i in range(0, len(lines), 2):
-        category_name = lines[i].split("Category (")[1].split("):")[0]
-        words = lines[i + 1].strip().split(' ')
-        category_embed = []
-        for word in words:
-            if word in embeddings:
-                category_embed.append(f"{word} {embeddings[word]}")
-        if category_name:
-            categories[category_name] = category_embed[:10]  # Considering the first 10 embeddings
+for category, embeds in word_embeddings.items():
+    if category in topic_embeddings:
+        categories[category] = [f"{category} {topic_embeddings[category]}\n{category} {embeds}\n"]
 
-# Save word embeddings for each category into separate files
+# Save embeddings for each category into separate files
 for category, embeds in categories.items():
-    filename = f"{category}_terms.txt"
+    filename = f"{category}.terms.txt"
     with open(filename, 'w') as file:
-        file.write('\n'.join(embeds))
+        file.write(''.join(embeds))
